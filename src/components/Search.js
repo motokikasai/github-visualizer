53433269917a0141c88eecf9b4ff4cf5592e12ee
motokikasai <kmomoky@hotmail.com>
2020-08-29 23:39:56 +0200

    Add Octocat at the right corner

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import IconOctocat from '../assets/github-icon-light.png';
+import Corner from './Corner.js';
 
 function Search() {
   return (
     <div className='search-container'>
+      <Corner />
       <div className='gh-icon'>
         <img src={IconOctocat} alt='Github icon' style={{ width: '60px' }} />
       </div>
