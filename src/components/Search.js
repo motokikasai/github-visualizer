316652b373a9eed14190186f4cbbe4644f6f979b
motokikasai <kmomoky@hotmail.com>
2020-08-29 16:29:18 +0200

    Set up structure of project

---
+++ src/components/Search.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import IconOctocat from '../assets/github-icon-light.png';
+
+export default function Search() {
+  return (
+    <div className='search-container'>
+      <div className='gh-icon'>
+        <img src={IconOctocat} alt='Github icon' style={{ width: '60px' }} />
+      </div>
+      <div className='search-label'>Find Your Favorite GitHub Profile</div>
+    </div>
+  );
+}
