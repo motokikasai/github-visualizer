f7e29ceb0f2ea1ffec40c2b215931840aa4d667a
motokikasai <kmomoky@hotmail.com>
2020-08-31 00:04:14 +0200

    Set up Routing basics

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import styled from 'styled-components';
 import IconOctocat from '../assets/github-icon-light.png';
 import Corner from '../components/Corner.js';
@@ -40,55 +41,30 @@ const HomeContainer = styled.div`
   }
 `;
 
-// const OctocatIcon = styled.img`
-//   width: 80px;
-//   padding: 20px;
-// `;
-
-// const SearchField = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   flex-direction: column;
-//   align-items: center;
-// `;
-
-// const SearchLabel = styled.div`
-//   font-size: 1.5rem;
-//   font-weight: bold;
-// `;
-
-// const SearchInput = styled.input`
-//   padding: 10px 28px;
-//   font-size: 2rem;
-//   margin: 14px;
-//   text-align: center;
-//   color: hotpink;
-//   border-style: none;
-//   border-radius: 5px;
-//   background: #331a33;
-//   &:focus {
-//     outline: none;
-//   }
-// `;
-
 function Home() {
+  const [username, setUsername] = useState('');
+  const handleChange = e => setUsername(e.target.value);
+
   return (
     <HomeContainer>
-      {/* <div className='search-container'> */}
       <Corner />
       <form
-        onSubmit={(e) => {
+        onSubmit={e => {
           e.preventDefault();
           // Routing logic...
+
+          setUsername('');
         }}
       >
-        {/* <SearchField className='gh-icon'> */}
         <img src={IconOctocat} alt='Github icon' />
-        <label>GitHub Handle/Profile</label>
-        <input type='text' />
-        {/* </SearchField> */}
+        <label htmlFor='username'>GitHub Handle/Profile</label>
+        <input
+          name='username'
+          type='text'
+          value={username}
+          onChange={handleChange}
+        />
       </form>
-      {/* </div> */}
     </HomeContainer>
   );
 }
